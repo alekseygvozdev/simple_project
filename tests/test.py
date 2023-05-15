b68ce89d4f4df5d21efef99d6350509d88bed25d
OleksiiYevtushenko <80823742+OleksiiYevtushenko@users.noreply.github.com>
2023-05-15 12:14:51 +0300

    Update test.py

--- tests/test.py
+++ tests/test.py
@@ -1,16 +1,21 @@
-
 import unittest
 
-class TestSimple(unittest.TestCase):
+def simple(x, y, name):
+    sum = x + y
+    result_str = f"Hello {name}, the sum of {x} and {y} is {sum}"
+    return sum, result_str
+
+class SimpleTest(unittest.TestCase):
     def test_simple(self):
-        x = 5
-        y = 10
+        # Проверяем, что сумма вычисляется правильно
+        x = 3
+        y = 5
         name = "John"
-        expected_sum = 15
-        expected_str = "Hello John, the sum of 5 and 10 is 15"
-        actual_sum, actual_str = simple(x, y, name)
-        self.assertEqual(expected_sum, actual_sum)
-        self.assertEqual(expected_str, actual_str)
+        expected_sum = 8
+        expected_result_str = f"Hello {name}, the sum of {x} and {y} is {expected_sum}"
+        actual_sum, actual_result_str = simple(x, y, name)
+        self.assertEqual(actual_sum, expected_sum)
+        self.assertEqual(actual_result_str, expected_result_str)
 
 if __name__ == '__main__':
     unittest.main()
