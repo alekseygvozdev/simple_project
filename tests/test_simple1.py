28073dc93b16810c51c43e35015476865f48c3a4
OleksiiYevtushenko <80823742+OleksiiYevtushenko@users.noreply.github.com>
2023-05-15 16:39:41 +0300

    Delete test_simple1.py

--- tests/test_simple1.py
+++
@@ -1,9 +0,0 @@
-
-from io import StringIO
-from unittest.mock import patch
-import example
-
-def test_hello_world(capsys):
-    example.hello_world()
-    captured = capsys.readouterr()
-    assert captured.out == "Hello, World!\n"
