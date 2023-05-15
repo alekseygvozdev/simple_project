a37c5744f32befbc322d6c111630c3c1db56a495
alekseygvozdev <57616507+alekseygvozdev@users.noreply.github.com>
2023-05-15 12:17:48 +0300

    OpenAI generated manual-test for the src/simple.py file, updated a file called test.py and populated it with content.

--- tests/test.py
+++ tests/test.py
@@ -1,21 +1,16 @@
+
 import unittest
 
-def simple(x, y, name):
-    sum = x + y
-    result_str = f"Hello {name}, the sum of {x} and {y} is {sum}"
-    return sum, result_str
-
-class SimpleTest(unittest.TestCase):
+class TestSimple(unittest.TestCase):
     def test_simple(self):
-        # Проверяем, что сумма вычисляется правильно
-        x = 3
-        y = 5
+        x = 5
+        y = 10
         name = "John"
-        expected_sum = 8
-        expected_result_str = f"Hello {name}, the sum of {x} and {y} is {expected_sum}"
-        actual_sum, actual_result_str = simple(x, y, name)
-        self.assertEqual(actual_sum, expected_sum)
-        self.assertEqual(actual_result_str, expected_result_str)
+        expected_sum = 15
+        expected_str = "Hello John, the sum of 5 and 10 is 15"
+        actual_sum, actual_str = simple(x, y, name)
+        self.assertEqual(expected_sum, actual_sum)
+        self.assertEqual(expected_str, actual_str)
 
 if __name__ == '__main__':
     unittest.main()
