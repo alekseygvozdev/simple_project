56eaa35476e8aa4af0823d973d2c434aac8f5949
OleksiiYevtushenko <80823742+OleksiiYevtushenko@users.noreply.github.com>
2023-05-15 13:52:45 +0300

    Update simple.py

--- src/simple.py
+++ src/simple.py
@@ -1,5 +1,6 @@
 # Python 3.7
-def simple(x, y, name):
-    sum = x + y
-    result_str = f"Hello {name}, the sum of {x} and {y} is {sum}"
-    return sum, result_str
+
+def hello_world():
+    print("Hello, World!")
+
+hello_world()
